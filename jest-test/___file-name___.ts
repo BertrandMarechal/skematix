c14b1e517f29f2328b9ef5e616f74404ef709050
BertrandMarechal <bertrand.marechal@gadz.org>
2021-09-11 23:45:15 +0100

    First commit

---
+++ jest-test/___file-name___.ts
@@ -0,0 +1,9 @@
+import { jest } from '@jest/globals';
+
+describe('___file-name___', function () {
+    describe('___function-name___', function () {
+        it('should return true', function () {
+            expect(true).toBeTruthy();
+        });
+    });
+});
